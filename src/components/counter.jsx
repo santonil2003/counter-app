8286b97ddb88f7d130d55bb7d7bfbd4bbc06e01b
Sanil <sanil@pcim.com.au>
2018-08-23 15:24:50 +1000

    itetration using map

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
 	state = {
-		count: 0,
-		imageUrl: 'https://picsum.photos/200'
+		count: 1,
+		imageUrl: 'https://picsum.photos/200',
+		tags: [ 'tag1', 'tag2', 'tag3' ]
 	};
 
 	styles = {
@@ -16,14 +17,22 @@ class Counter extends Component {
 		return count === 0 ? 'Zero' : count;
 	}
 
+	getBadgeClasses() {
+		if (this.state.count > 0) {
+			return 'badge btn btn-primary';
+		} else {
+			return 'badge btn btn-warning';
+		}
+	}
+
 	render() {
 		return (
 			<div>
-				<img src={this.state.imageUrl} alt="" />
-				<span style={this.styles} className="badge badge-primary m-2">
-					{this.formatCount()}
-				</span>
+				<img src={this.state.imageUrl} alt="" style={{ display: 'none' }} />
+				<span className={this.getBadgeClasses()}>{this.formatCount()}</span>
 				<button className="btn btn-sm">Increment</button>
+
+				<ul>{this.state.tags.map((tag) => <li key={tag}>{tag}</li>)}</ul>
 			</div>
 		);
 	}
